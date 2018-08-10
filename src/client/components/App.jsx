bf63fcd69879dd05d4385d67be1b976f00a26142
Stanley Makazhe <devt3000@gmail.com>
2018-08-10 13:35:52 +0200

    Create /src/client/components/App.jsx

---
+++ src/client/components/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                {'You are now runnning React on Express'}
+            </div>
+        )
+    }
+}
+
+export default App
